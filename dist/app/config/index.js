4482935fe1a6e0245af788d400f1f8e05d38c4d4
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-12-21 00:46:54 +0600

    Refector config file

--- dist/app/config/index.js
+++ dist/app/config/index.js
@@ -12,7 +12,5 @@ exports.default = {
     databaseUrl: process.env.DATABASE_URL,
     bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
-    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
     jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-    jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
 };
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,7 +9,5 @@ export default {
   databaseUrl: process.env.DATABASE_URL,
   bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
-  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
   jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
 };
